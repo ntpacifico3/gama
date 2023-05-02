82ddc1a6f4bbd9279875046e27cc0406e4fcbcce
n0sLen <ntpacifico@ntpacifico.com>
2023-05-02 14:00:12 +0800

    update

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const body = document.querySelector("body"),
         modeSwitch = body.querySelector(".toggle-switch"),
         modeText = body.querySelector(".mode-text");
 
-        modeSwitch.addEventListener("click", ()=> {
+        toggle.addEventListener("click", ()=> {
+            sidebar.classList.toggle("close");
+        });
+
+
+         modeSwitch.addEventListener("click", ()=> {
             body.classList.toggle("dark");
-        })
+
+            if(body.classList.contains("dark")) {
+                modeText.innerText = "Ligh Mode"
+            } else {
+                modeText.innerText = "Dark Mode"
+            }
+
+        });
