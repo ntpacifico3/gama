05912d172adc0c0550de98d97152a625846280bf
Nelson Tumulak Pacifico <ntpacifico@ntpacifico.com>
2023-05-04 12:40:14 +0800

    update

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const body = document.querySelector("body"),
-        sidebar = body.querySelector(".sidebar"),
-        toggle = body.querySelector(".toggle"),
-        searchBtn = body.querySelector(".search-box"),
-        modeSwitch = body.querySelector(".toggle-switch"),
-        modeText = body.querySelector(".mode-text");
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
-        toggle.addEventListener("click", ()=> {
-            sidebar.classList.toggle("close");
-        });
 
-        searchBtn.addEventListener("click", () => {
-            sidebar.classList.remove("close");
-        })
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
 
-         modeSwitch.addEventListener("click", ()=> {
-            body.classList.toggle("dark");
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
 
-            if(body.classList.contains("dark")) {
-                modeText.innerText = "Ligh Mode"
-            } else {
-                modeText.innerText = "Dark Mode"
-            }
-
-        });
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
